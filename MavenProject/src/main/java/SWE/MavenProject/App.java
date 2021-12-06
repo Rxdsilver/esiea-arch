80c43c78b676808788e01bbeb209376555c0d70a
Rxdsilver <63789937+Rxdsilver@users.noreply.github.com>
2021-12-06 15:34:53 +0100

    Exercise

--- MavenProject/src/main/java/SWE/MavenProject/App.java
+++ MavenProject/src/main/java/SWE/MavenProject/App.java
@@ -1,13 +1,39 @@
 package SWE.MavenProject;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	
+    public static void main( String[] args ){	
+    	
+ 
+    }
+    
+    public String getReverse(String[] array) {
+	    char[] ch = array[0].toCharArray();
+	    	  
+	    for(int i=0; i<(ch.length)/2;i++) {
+	       char temp = ch[ch.length-i-1];
+	       ch[ch.length-i-1] = ch[i];
+	       ch[i] = temp;
+	    }
+	        
+	    return String.valueOf(ch);
+    }
+    
+    public String getMaximum(String[] array) {
+    	Integer ret = Integer.parseInt(array[0]);
+    	
+        for (int i = 0; i< array.length ;i++) {
+     	   if (Integer.parseInt(array[i])>ret) {
+     		   ret = Integer.parseInt(array[i]);
+     	   }
+        }     
+        return ret.toString();
+    }
+    
+    public String getCube(String[] array) {
+    	Integer temp = Integer.parseInt(array[0]);
+    	temp = temp*temp*temp;
+    	return String.valueOf(temp);
     }
 }
--- MavenProject/src/test/java/SWE/MavenProject/AppTest.java
+++ MavenProject/src/test/java/SWE/MavenProject/AppTest.java
@@ -1,20 +1,25 @@
 package SWE.MavenProject;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest extends App
 {
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+    	String[] args1 = {"2","9","30","1","20"};
+    	assertEquals("30", getMaximum(args1));
+    	
+    	String[] args2 = {"Follow"};
+    	assertEquals("wolloF", getReverse(args2));
+    	
+    	String[] args3 = {"2"};
+    	assertEquals("8", getCube(args3));
     }
 }
